624a307525a77410f08e74a4cdda093e914835d9
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-05-22 22:45:39 -0300

    condição para ver se já existem tarefas adicionadas para evitar a animação dos itens

--- src/javascript/animate.js
+++ src/javascript/animate.js
@@ -1,13 +1,24 @@
 const elements = gsap.utils.toArray("[data-animate='main']");
+const tarefas = gsap.utils.toArray("[data-animate='tarefa']");
 const timeline = gsap.timeline();
-timeline.fromTo(
-  elements,
-  { opacity: 0, x: -32 },
-  { opacity: 1, x: 0, stagger: 0.2 }
-);
-timeline.fromTo(
-  '[data-animate="icon"]',
-  { opacity: 0, x: -32 },
-  { opacity: 1, x: 0, stagger: 0.15 }
-);
-
+const formElements = document.querySelectorAll("div.task");
+if (formElements.length == 0) {
+  timeline.fromTo(
+    elements,
+    { opacity: 0, x: -32 },
+    { opacity: 1, x: 0, stagger: 0.2 }
+  );
+  timeline.fromTo(
+    '[data-animate="icon"]',
+    { opacity: 0, x: -32 },
+    { opacity: 1, x: 0, stagger: 0.15 }
+  );
+}
+else if(formElements.length > 0){
+  timeline.fromTo(
+    tarefas,
+    { opacity: 0, x: -32 },
+    { opacity: 1, x: 0, stagger: 0.2 }
+  );
+  console.log('alo')
+}
